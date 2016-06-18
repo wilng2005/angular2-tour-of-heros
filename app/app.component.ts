688078fc5fe70306b66c46e5e202bdd42b7b6504
Wil Ng <wil.ng.2005@gmail.com>
2016-06-18 15:01:02 +0800

    end of services page

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {HeroDetailComponent } from './hero-detail.component';
 import {Hero} from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
@@ -64,28 +66,24 @@ import {Hero} from './hero';
         border-radius: 4px 0 0 4px;
       }
     `],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
-    public heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
+
+    constructor(private heroService: HeroService) { }
+    ngOnInit() {
+        this.getHeroes();
+    }
+    getHeroes() {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 
     onSelect(hero: Hero){
         this.selectedHero=hero;
 
     }
 }
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
